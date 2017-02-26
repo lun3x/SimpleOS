2f53515183384e78cf25d325f8a277336730413f
Louis <louiswyborn@gmail.com>
2017-02-26 15:50:36 +0000

    Added scheduler, pcb and ctx code

--- kernel/hilevel.c
+++ kernel/hilevel.c
@@ -1,6 +1,44 @@
 #include "hilevel.h"
 
-void hilevel_handler_rst() {
+/* Since we *know* there will be 2 processes, stemming from the 2 user
+ * programs, we can
+ *
+ * - allocate a fixed-size process table (of PCBs), and use a pointer
+ *   to keep track of which entry is currently executing, and
+ * - employ a fixed-case of round-robin scheduling: no more processes
+ *   can be created, and neither is able to complete.
+ */
+
+pcb_t pcb[ 3 ], *current = NULL;
+
+void scheduler( ctx_t* ctx ) {
+  if      ( current == &pcb[ 0 ] ) {
+    memcpy( &pcb[ 0 ].ctx, ctx, sizeof( ctx_t ) ); // preserve P_1
+    memcpy( ctx, &pcb[ 1 ].ctx, sizeof( ctx_t ) ); // restore  P_2
+    current = &pcb[ 1 ];
+  }
+  else if ( current == &pcb[ 1 ] ) {
+    memcpy( &pcb[ 1 ].ctx, ctx, sizeof( ctx_t ) ); // preserve P_2
+    memcpy( ctx, &pcb[ 2 ].ctx, sizeof( ctx_t ) ); // restore  P_3
+    current = &pcb[ 2 ];
+  }
+  else if ( current == &pcb[ 2 ] ) {
+    memcpy( &pcb[ 2 ].ctx, ctx, sizeof( ctx_t ) ); // preserve P_3
+    memcpy( ctx, &pcb[ 0 ].ctx, sizeof( ctx_t ) ); // restore  P_1
+    current = &pcb[ 0 ];
+  }
+
+  return;
+}
+
+extern void     main_P3();
+extern uint32_t tos_P3;
+extern void     main_P4();
+extern uint32_t tos_P4;
+extern void     main_P5();
+extern uint32_t tos_P5;
+
+void hilevel_handler_rst(ctx_t* ctx) {
   /* Configure the mechanism for interrupt handling by
    *
    * - configuring timer st. it raises a (periodic) interrupt for each
@@ -21,12 +59,44 @@ void hilevel_handler_rst() {
   GICC0->CTLR         = 0x00000001; // enable GIC interface
   GICD0->CTLR         = 0x00000001; // enable GIC distributor
 
+  /* Initialise PCBs representing processes stemming from execution of
+   * the two user programs.  Note in each case that
+   *
+   * - the CPSR value of 0x50 means the processor is switched into USR
+   *   mode, with IRQ interrupts enabled, and
+   * - the PC and SP values matche the entry point and top of stack.
+   */
+
+  memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );
+  pcb[ 0 ].pid      = 1;
+  pcb[ 0 ].ctx.cpsr = 0x50;
+  pcb[ 0 ].ctx.pc   = ( uint32_t )( &main_P3 );
+  pcb[ 0 ].ctx.sp   = ( uint32_t )( &tos_P3  );
+
+  memset( &pcb[ 1 ], 0, sizeof( pcb_t ) );
+  pcb[ 1 ].pid      = 2;
+  pcb[ 1 ].ctx.cpsr = 0x50;
+  pcb[ 1 ].ctx.pc   = ( uint32_t )( &main_P4 );
+  pcb[ 1 ].ctx.sp   = ( uint32_t )( &tos_P4  );
+
+  memset( &pcb[ 2 ], 0, sizeof( pcb_t ) );
+  pcb[ 2 ].pid      = 3;
+  pcb[ 2 ].ctx.cpsr = 0x50;
+  pcb[ 2 ].ctx.pc   = ( uint32_t )( &main_P5 );
+  pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_P5  );
+
+  /* Once the PCBs are initialised, we (arbitrarily) select one to be
+   * restored (i.e., executed) when the function then returns.
+   */
+
+  current = &pcb[ 0 ]; memcpy( ctx, &current->ctx, sizeof( ctx_t ) );
+
   int_enable_irq();
 
   return;
 }
 
-void hilevel_handler_irq() {
+void hilevel_handler_irq(ctx_t* ctx) {
   // Step 2: read  the interrupt identifier so we know the source.
 
   uint32_t id = GICC0->IAR;
@@ -35,6 +105,7 @@ void hilevel_handler_irq() {
 
   if( id == GIC_SOURCE_TIMER0 ) {
     PL011_putc( UART0, 'T', true ); TIMER0->Timer1IntClr = 0x01;
+
   }
 
   // Step 5: write the interrupt identifier to signal we're done.
